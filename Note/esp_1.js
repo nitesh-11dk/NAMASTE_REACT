3cbc16b01fbd0bfed65bf466f8babf59c210af63
Nitesh-DK <kushwahandk@gmail.com>
2024-06-08 20:57:28 +0530

    lec 1 completed

--- Note/esp_1.js
+++ Note/esp_1.js
@@ -2,5 +2,7 @@
 // ! hello world , in html , js , and react
 
 //  ! cdn ,  of react
-// 2 cdn - 
-//  1. core react , and another is Dom- react 
+// 2 cdn -
+//  1. core react , and another is Dom- react
+
+//  1. react method called React.createElement at the end it is basically an  js object   
--- lectures/lec1/script.js
+++ lectures/lec1/script.js
@@ -1,4 +1,15 @@
-const heading = React.createElement("h1", {}, "hello world form react !!!")
+// const heading = React.createElement("h1", {id:"heading"  , class:"bg-red"}, "hello world form react !!!")
+
+
+// const heading = React.createElement("h1", {id:"heading"  , class:"bg-red"},
+//     React.createElement("h1", {}, React.createElement("h1", {}, "this is the innerest h1 tag"))
+// )
+
+//  ! if you have to make the sbiling , 3rd property -- as [ ]
+// const heading = React.createElement("h1", {id:"heading"  , class:"bg-red"},
+//     React.createElement("h3", {}, [React.createElement("h3", {}, "this is the innerest h3 tag") ,React.createElement("h3", {}, "this is the innerest h3 tag 1")])
+// )
+
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
