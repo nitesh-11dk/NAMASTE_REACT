a812a50c4f548cc7c7130095f37c87a754c9e738
Nitesh-DK <kushwahandk@gmail.com>
2024-06-08 20:29:23 +0530

    project initialize

---
+++ Note/esp_1.js
@@ -0,0 +1,6 @@
+// ! Esp -1
+// ! hello world , in html , js , and react
+
+//  ! cdn ,  of react
+// 2 cdn - 
+//  1. core react , and another is Dom- react 
---
+++ lectures/lec1/script.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {}, "hello world form react !!!")
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(heading);
